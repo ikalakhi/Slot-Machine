f384ffdbc3417ae3416c16b6c637dfae8d6d5197
ikalakhi <imranekalakhi13@gmail.com>
2024-02-25 15:09:29 +0100

    project setup

---
+++ project.js
@@ -0,0 +1,7 @@
+//1. deposet some of money
+//2. Determine numer of lines to bet on
+//3. Collect a bet amount
+//4. spin the slot machine
+//5. check if the user won
+//6. give the user thier their winnings
+//7. play again
