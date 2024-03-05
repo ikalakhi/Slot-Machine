646ee7d4d15d28893be7e76fe12d468b08e64e0c
ikalakhi <imranekalakhi13@gmail.com>
2024-03-05 13:10:24 +0100

    getting number of lines and bet

--- project.js
+++ project.js
@@ -5,3 +5,51 @@
 //5. check if the user won
 //6. give the user thier their winnings
 //7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while(true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalide depost amount, try again. ");
+        } else {
+            return numberDepositAmount;
+        }
+    }
+}
+
+const getNumberOfLines = () => {
+    while(true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalide number of lines, try again. ");
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+const getBet = (balance, lines) => {
+    while(true) {
+        const bet = prompt("Enter bet per line: ");
+        const numberBet = parseFloat(bet);
+        
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalide bet, try again. ");
+        } else {
+            return numberBet;
+        }
+    }
+}
+
+
+let balance = deposit();
+const numberofLines = getNumberOfLines();
+const getbet = getBet(balance, numberofLines);
+
+console.log(numberofLines, getbet);
