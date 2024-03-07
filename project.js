1126022504e89cc9a2b5a6eccef2613bedb69000
ikalakhi <imranekalakhi13@gmail.com>
2024-03-07 20:06:55 +0100

    transpose the reels

--- project.js
+++ project.js
@@ -72,8 +72,9 @@ const spin = () => {
         }
     }
 
-    const reels = [[], [], []];
+    const reels = [];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -85,10 +86,21 @@ const spin = () => {
     return reels;
 }
 
-console.log(spin());
+const transpose = (reels) => {
+    const rows = [];
+
+    for(let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+}
+
 
 let balance = deposit();
 const numberofLines = getNumberOfLines();
 const getbet = getBet(balance, numberofLines);
 
-console.log(numberofLines, getbet);
+console.log(spin());
